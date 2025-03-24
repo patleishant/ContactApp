ca5528bdb7d9a6f302c18fef88ce264d0a9ab827
IshantPatle <ishantpatle09@gmail.com>
2025-03-24 10:37:10 +0530

    new file created

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,67 @@
-
-import Navbar from './compenents/Navbar'
-
+import { useEffect, useState } from "react";
+import Navbar from "./compenents/Navbar";
+import { CiSearch } from "react-icons/ci";
+import { CiCirclePlus } from "react-icons/ci";
+import {collection,getDocs} from 'firebase/firestore';
+import {db} from "./config/firebase";
+import {HiOutlineUserCircle} from 'react-icons/hi'
 const App = () => {
 
+  const  [contacts, setContacts] = useState([]);
+
+  useEffect(()=> {
+    const getContacts = async() => {
+
+      try {
+        const contactsRef = collection(db , "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => 
+         {
+          return{
+          id:doc.id,
+          ...doc.data()
+          };
+        });
+
+        setContacts( contactLists);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getContacts();
+  },[]);
+
   return (
-   <div className="mx-auto max-w-[370px]">
-    <Navbar/>
-    <div>
-      <input type="text" className="rounded-md border border-white bg-transparent" id="" />
+    <div className="mx-auto max-w-[370px] px-4">
+      <Navbar />
+      <div className="flex gap-2">
+      <div className="relative flex item-center">
+        <CiSearch className="absolute ml-1 text-3xl text-white" />
+        <input
+          type="text"
+          className=" h-10 flex-grow  
+          rounded-md border border-white bg-transparent pl-9 text-white"
+          id=""
+        />
+      </div>
+     
+      <CiCirclePlus className="text-5xl cursor-pointer text-white" />
+     
+      </div>
+
+      <div>
+        {
+          contacts.map((contcat) => 
+          <div key={contcat.id}>
+            <HiOutlineUserCircle />
+            <div><h2></h2>
+              <p></p>
+            </div>
+          </div>
+          )}
+      </div>
     </div>
-   </div>
   );
 };
 
