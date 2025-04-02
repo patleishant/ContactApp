df39e3764d57b157a26137c3e05e0cb9ccd4191f
IshantPatle <ishantpatle09@gmail.com>
2025-04-02 21:24:31 +0530

    updated

--- my-project/src/compenents/Modal.jsx
+++ my-project/src/compenents/Modal.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import {createPortal} from "react-dom"
+import { createPortal } from "react-dom";
 import { AiOutlineClose } from "react-icons/ai";
 
-const Modal =({onClose,isOpen,children}) => {
-  return createPortal (<>{ isOpen &&(
-      <>
-      <div className=" relative z-50 m-auto min-h-[200px] max-w-[80%] bg-white">
-        <div className='flex justify-end'>
-            <AiOutlineClose onClick ={onClose} className =" self-end text-2xl"/>
+const Modal = ({ onClose, isOpen, children }) => {
+  return createPortal(
+    <>
+      {isOpen && (
+        <div className="absolute top-0 z-40 grid h-screen w-screen place-items-center backdrop-blur">
+          <div className="relative z-50 m-auto min-h-[200px] min-w-[80%] bg-white p-4">
+            <div className="flex justify-end">
+              <AiOutlineClose onClick={onClose} className="self-end text-2xl" />
+            </div>
+            {children}
+          </div>
         </div>
-        {children}
-        </div> 
-        <div onClick={onClose} className=' absolute top-0 z-40 h-screen w-screen '/>
-      </>
-    )} 
-    </>
-  , document.getElementById("modal-root"));
+      )}
+    </>,
+    document.getElementById("modal-root")
+  );
 };
 
 export default Modal;
